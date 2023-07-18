e76355ae646c96ed2d521b8d9d742b3b8a112ec0
Nadia Idris <nadia@fasterlap.com>
2023-07-17 20:03:17 -0700

    Add spreadsheet data to spreadsheet.json file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 // Import express library.
+import bodyParser from "body-parser";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
@@ -7,6 +8,9 @@ dotenv.config();
 
 // Initialize the express engine.
 const app: Express = express();
+
+// body-parser middeleware to parse the request body
+app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 
 // Routes:
 // Define default endpoint.
@@ -24,7 +28,10 @@ app.get("/sp/get", (request: Request, response: Response) => {
       fs.mkdirSync("./data");
     }
     if (!fs.existsSync("./data/spreadsheet.json")) {
-      fs.writeFileSync("./data/spreadsheet.json", "{}");
+      fs.writeFileSync("./data/spreadsheet.json", "{}", (error: Error) => {
+        if (error)
+          console.error("Error trying to write file sync: --> ", error);
+      });
     }
 
     // Note: If the file starts with "./" it is considered a relative file to the file that called require.
@@ -40,13 +47,14 @@ app.get("/sp/get", (request: Request, response: Response) => {
 app.post("/sp/set", (request: Request, response: Response) => {
   // Try to save spreadsheet.json file with the data from the user's file system.
   // If it doesn't exist, make it and return an empty spreadsheet.json file.
-  try {
+	try {
+		console.log(request.body)
     const fs = require("fs");
-    fs.writeFileSync("spreadsheet.json", request.body);
-    response.send("Success");
+		const data = Buffer.alloc(JSON.stringify(request.body).length, JSON.stringify(request.body))
+		fs.writeFileSync("./data/spreadsheet.json", data);
+    response.send("Success ---> spreadsheet.json file saved.");
   } catch (err: any) {
-    console.log("Error: " + err);
-    response.send("Error: " + err);
+    response.send("Nadia's  Error: " + err);
   }
 });
 
